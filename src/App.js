f806281f9185fc3a952c24d3e43a1aa406344e85
notGonim <mhmodgonim96@gmail.com>
2021-02-16 20:28:14 +0200

    Adding router intilization for the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+import { Home } from './pages/Home';
+
+
 function App() {
 
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
